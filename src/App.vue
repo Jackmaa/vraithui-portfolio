8f8789609e5fe0734d860bfa7905ea6c39c6e431
Jackmaa <valentingillot@gmail.com>
2025-11-04 09:13:34 +0100

    Add @vercel/speed-insights dependency and integrate SpeedInsights component in App.vue for performance monitoring

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
+  <SpeedInsights />
   <EditorShell />
 </template>
 
 <script setup>
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import EditorShell from "./vraithui/layout/EditorShell.vue";
 </script>
