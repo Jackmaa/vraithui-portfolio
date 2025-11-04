1459ff19a2ace8a8fd064c18dfa89bccaf2b478f
Jackmaa <valentingillot@gmail.com>
2025-11-04 09:26:52 +0100

    Remove @vercel/analytics and @vercel/speed-insights dependencies along with their components from App.vue to streamline the application and reduce unnecessary bloat.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <SpeedInsights />
-  <Analytics />
   <EditorShell />
 </template>
 
 <script setup>
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import EditorShell from "./vraithui/layout/EditorShell.vue";
 </script>
